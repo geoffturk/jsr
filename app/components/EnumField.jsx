04f0c77782ab4a734414d5ce6e37e9b7e23c9202
Geoff Turk <geoff@ic3.dev>
2022-04-08 10:58:14 +0200

    Default blank in dropdowns, CSS tweaks

--- app/components/EnumField.jsx
+++ app/components/EnumField.jsx
@@ -10,6 +10,7 @@ export default function EnumField({
       <label>
         <span className="key">{title}:</span>
         <select name={name} id={name} multiple={multi}>
+          {multi ? null : <option value="" key="0"></option>}
           {enumList.map((item, index) => (
             <option value={item} key={item}>
               {enumNamesList ? enumNamesList[index] : item}
